72753a95e793dbbfa22aaa7157b357f23f747465
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-19 17:39:25 -0500

    tarea

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ resultado = parseInt(numero1) + parseInt(numero2);
 alert (resultado);
 
 
+
+
+/*
+ * Complete the function below.
+ */
+function promedio(num1, num2, num3) {
+    var suma = num1 + num2 +num3;
+    var promedio = suma/3;
+    return promedio;
+
+}
+
+function sueldoSemanal(horas, pago) {
+    return horas * pago;
+    
+    
+
+
+}
+
+/*
+ * Complete the function below.
+ */
+function costoLlamada(tiempo, costoMinuto) {
+return tiempo * costoMinuto;
+
+}
+
+
